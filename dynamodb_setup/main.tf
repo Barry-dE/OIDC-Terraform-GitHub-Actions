1936d80567f5eb9aa8bceb5527b2e1113ca66f3e
Barigbue Nbira <barigbuenbira@gmail.com>
2025-10-13 21:48:31 +0100

    chore(ci): add workflow to create dynamoDB for terraform state lcoking
    Signed-off-by: Barigbue Nbira <barigbuenbira@gmail.com>

---
+++ dynamodb_setup/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "local" {} # just for this setup step
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
